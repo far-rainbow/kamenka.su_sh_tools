958fe9ff78b093e15a1789d75ba239e5a2025ff8
Pavel E. Petrov <admin@kamenka.su>
2018-09-29 16:49:27 +0300

    coloring and history script added

---
+++ bashrc.sh
@@ -0,0 +1,10 @@
+#~/bin/sh
+
+# CSH FREEBSD-LIKE HISTORY (i prefer this becouse of FBSD youth)
+PROMPT_COMMAND='history -a'
+bind '"\e[A": history-search-backward'
+bind '"\e[B": history-search-forward'
+
+# VERY USEFULL COLORING FOR THOSE WHO HAVE TONS OF CONSOLES IN USE
+PS1='\[\e]0;[\w]\a\r\e[1;38;5;118;48;5;16m\] \u \[\e[38;5;16;48;5;118m\] \h \[\e[0m\] \$'
+
