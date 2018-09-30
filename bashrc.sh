544816cc9cf14f583bb7edb047e95be31b9fae13
Pavel E. Petrov <p_13@mail.ru>
2018-09-30 05:35:32 +0300

    gramar fix

--- bashrc.sh
+++ bashrc.sh
@@ -10,4 +10,4 @@ PS1='\[\e]0;[\w]\a\r\e[1;38;5;118;48;5;16m\] \u \[\e[38;5;16;48;5;118m\] \h \[\e
 
 #
 # OF COURSE IT IS BETTER TO PLACE THIS IN ~/.bashrc or similar config!
-# make it yourself
+# do it yourself
